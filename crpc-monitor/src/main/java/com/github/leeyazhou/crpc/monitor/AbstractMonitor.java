84f99d626f753fa8be19b9f60da50b802be54055
leeyazhou <lee_yazhou@163.com>
2020-05-13 14:13:55 +0800

    build: refactor

--- crpc-monitor/src/main/java/com/github/leeyazhou/crpc/monitor/AbstractMonitor.java
+++ crpc-monitor/src/main/java/com/github/leeyazhou/crpc/monitor/AbstractMonitor.java
@@ -13,10 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/**
- * 
- */
-
 package com.github.leeyazhou.crpc.monitor;
 
 import com.github.leeyazhou.crpc.protocol.Response;
